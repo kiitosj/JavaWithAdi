cb8b304815ad4cf92713d5b03b836a84a2d256f1
kiitosj <kiitosjasper@gmail.com>
2020-10-29 10:31:08 +0100

    my fist commit

---
+++ gitexerciseclass/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        MyClass myObj1 = new MyClass();  // Object 1
+        MyClass myObj2 = new MyClass();  // Object 2
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+
+
+
+    }
+}
---
+++ gitexerciseclass/src/com/company/MyClass.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class MyClass {
+
+    int x = 10;
+
+}
